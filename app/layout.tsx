4fd565a56b20d17c2bc6b5aeb375a43e70c6743c
Sujal Maharjan <sujalmaharjan36@gmail.com>
2026-08-28 16:25:31 +0545

    fix: layout fix of app

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
       suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col" suppressHydrationWarning>
+      <body className="h-full flex flex-col" suppressHydrationWarning>
         <Providers>{children}</Providers>
       </body>
     </html>
